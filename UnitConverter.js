354c8049b219bdef2a066ca02160f8676bf0207d
Hamid Reza Ghavami <hamidr.ghavami@gmail.com>
2025-09-26 17:03:57 +0300

    first changes

---
+++ UnitConverter.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import { convertLength, convertWeight, convertTemperature } from './convert';
+import router from 'express';
+
+const app = express();
+
+
+// welcome router
+app.get('/', (req, res) => { 
+    res.send("Welcome to the Unit Converter website!");
+});
+
+// length router
+app.get('/length/:value/:from/:to', (req, res) => { 
+    const { value, from, to } = req.params;
+
+    try { 
+        const result = convertLength(parseFloat(value), from, to);
+        res.status(200).send(`Result of your calculation: ${result} ${to}`);
+    } catch (err) { 
+        res.status(400).send(err.message);
+    }
+});
+
+// weight router
+app.get('/weight/:value/:from/:to', (req, res) => { 
+    const {value, from, to} = req.params;
+
+    try { 
+        const result = convertWeight(parseFloat(value), from, to);
+        res.status(200).send(`Result of your calculation: ${result} ${to}`);
+    } catch (err) { 
+        res.status(400).send(err.message);
+    }
+});
+
+// porgrams continue
+
+module.exports = router;
